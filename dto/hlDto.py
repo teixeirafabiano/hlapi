d4d45348bb768f9a722af61e53127b34e5f84da8
FABIANO GODOIS TEIXEIRA <44817364+teixeirafabiano@users.noreply.github.com>
2023-01-18 22:15:31 -0300

    Add files via upload

---
+++ dto/hlDto.py
@@ -0,0 +1,34 @@
+from typing import List, Union
+from pydantic import BaseModel
+
+class Order(BaseModel):
+	zipcode: str
+	service: str
+	project: str
+	describe: str
+	emergency: str
+	descrepair: List[str] = []
+	replace: List[str] = []
+	location: str
+	status: str
+	completed: str
+	street: str
+	city: str
+	f_name: str
+	l_name: str
+	phone: str
+	email: str
+
+external_data = {
+    "zipcode": "37915",
+    "service": "plumbing",
+    "descrepair": ["not work", "leaking pipe", "slow drain"]
+}
+
+#order = Order(**external_data)
+#print(order.service)
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
---
+++ main.py
@@ -0,0 +1,50 @@
+from dto.hlDto import *
+from typing import List, Union
+
+from fastapi import FastAPI, Request
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseModel
+
+app = FastAPI(    
+	title="Hub for Labor API",
+    description="This API has the necessary CRUD to manipulate HL platform services.",
+    version="0.0.1",
+    terms_of_service="https://hubforlabor.com/terms/",
+    contact={
+        "name": "Teixeira, Fabiano",
+        "url": "https://hubforlabor.com/contact/",
+        "email": "teixeirafabiano@gmail.com",
+    }
+)
+
+#@app.post("/")
+#async def read_root():
+#    return {"Hello": "World"}
+#
+#@app.get("/")
+#async def read_root():
+#    return {"Hello": "World"}
+
+@app.post("/status/{status_id}")
+def status(status_id: str, request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host, "status_id": status_id}
+
+#@app.post("/order/{order_id}")
+#def read_root(order_id: str, request: Request):
+#    client_host = request.client.host
+#    return {"client_host": client_host, "order_id": order_id}
+#
+#@app.get("/items/{item_id}")
+#async def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
+
+#@app.put("/items/{item_id}")
+#async def update_item(item_id: int, item: Item):
+#    return {"item_name": item.name, "item_id": item_id}
+
+#@app.get("/orders/{order_id}")
+#async def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
