f9c5ad610ee0f2a7993d7d0e3e23c2c34a1bbed5
FABIANO GODOIS TEIXEIRA <44817364+teixeirafabiano@users.noreply.github.com>
2023-04-16 21:39:22 -0300

    Add files via upload

---
+++ dlo/HlDLO.py
@@ -0,0 +1,54 @@
+import argparse
+import os
+import sys
+import pandas
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+print(sys.path, os.getcwd())
+
+
+#from app.DTO.CiclistaDTO import CiclistaDTO
+from dao.HlDAO import HlDAO
+
+class HlDLO:
+
+    def __init__(self):
+        pass
+
+    # def selectById(self, table, column, value):
+    #     objDao = HlDAO()
+    #     lstCiclista = []
+
+    #     try:
+    #         result = objDao.select(table, column, value)
+    #         for i in range(0, len(result)):
+    #             lstCiclista.append(CiclistaDTO(result[i][0], result[i][1], result[i][2], result[i][3], result[i][4]))
+
+    #     except Exception as exc:
+    #         print(f"Erro ao buscar {table} pelo id!", exc)
+    #     return lstCiclista
+
+    def selectWorkerByService(self, service, value):
+        objDao = HlDAO()
+        #lstCiclista = []
+        result = []
+        try:
+            result = objDao.selectWorkerByService(service, "ini_zipcode", "end_zipcode", value)
+            # print("=======", result[0][0])
+            # for i in range(0, len(result)):
+            #     lstCiclista.append(CiclistaDTO(result[i][0], result[i][1], result[i][2], result[i][3], result[i][4]))
+
+        except Exception as exc:
+            print(f"Erro ao na consulta!", exc)
+        return str(result[0][0]) #lstCiclista
+
+if __name__ == '__main__':
+    objDlo = HlDLO()
+    lst = []
+
+    qt = objDlo.selectWorkerByService("Plumbing", "90007")
+    print(type(qt), qt)
+
+
---
+++ dlo/ServiceDLO.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+import sys
+import pandas
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+print(sys.path, os.getcwd())
+
+
+from dto.ServiceDTO import ServiceDTO
+from dao.ServiceDAO import ServiceDAO
+
+class ServiceDLO:
+
+    def __init__(self):
+        pass
+
+    def selectService(self, value):
+        objDao = ServiceDAO()
+        lstService = []
+        result = []
+        try:
+            result = objDao.selectService(value)
+            print("=======", result[0][0])
+            for i in range(0, len(result)):
+                print(result[i][0], result[i][1], result[i][2], result[i][4], result[i][3])
+                lstService.append(ServiceDTO(result[i][0], result[i][1], result[i][2], result[i][4], result[i][3]))
+        
+        except Exception as exc:
+            print(f"Query on Service with error...", exc)
+        return lstService
+
+    def insertService(self, nm_service, jn_service):
+        objDao = ServiceDAO()
+        nu_id = 0
+        try:
+            nu_id = int(objDao.insert(nm_service, jn_service))
+            # print("=======", result[0][0])
+            # for i in range(0, len(result)):
+            #     print(result[i][0], result[i][1], result[i][2], result[i][4], result[i][3])
+            #     lstService.append(ServiceDTO(result[i][0], result[i][1], result[i][2], result[i][4], result[i][3]))
+        
+        except Exception as exc:
+            print(f"Query on Service with error...", exc)
+        return nu_id
+
+
+if __name__ == '__main__':
+    objDlo = ServiceDLO()
+    lst = []
+
+    lst.append('tbl_service')
+    lst.append('nu_id')
+    lst.append('1')
+
+    qt = objDlo.selectService(lst)
+    print(type(qt), qt)
+
+
