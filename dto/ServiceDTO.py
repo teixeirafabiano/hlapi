f7e1244a22acf8a1d9832741ddac534e40d657f6
FABIANO GODOIS TEIXEIRA <44817364+teixeirafabiano@users.noreply.github.com>
2023-04-16 21:39:43 -0300

    Add files via upload

---
+++ dto/ServiceDTO.py
@@ -0,0 +1,8 @@
+class ServiceDTO:
+
+    def __init__(self, nu_id, nm_service, jn_service, dt_service, st_service):
+        self.nu_id = nu_id
+        self.nm_service = nm_service
+        self.jn_service = jn_service
+        self.dt_service = dt_service
+        self.st_service = st_service
