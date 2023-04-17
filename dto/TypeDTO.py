a8c20d37759fbf7fa926254cbc209022ac18a359
FABIANO GODOIS TEIXEIRA <44817364+teixeirafabiano@users.noreply.github.com>
2023-04-17 10:43:13 -0300

    Add files via upload

---
+++ dto/TypeDTO.py
@@ -0,0 +1,7 @@
+class TypeDTO:
+
+    def __init__(self, nu_id_category, nm_category, nu_id_type, nm_type):
+        self.nu_id_category = nu_id_category
+        self.nm_category = nm_category
+        self.nu_id_type = nu_id_type
+        self.nm_type = nm_type
